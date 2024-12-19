f3664aa66bc1be5280f4d58dc623c01b40658641
Chester <chestermaligaso29@gmail.com>
2024-12-19 13:12:02 +0800

    initial commit

---
+++ .history/frontend/src/App_20241219130215.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-lg font-semibold">Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ .history/frontend/src/App_20241219131058.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-lg font-semibold text-black">Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
